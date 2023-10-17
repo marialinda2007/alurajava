6d18968c723a0328988e090d4f99af7e97ebf043
marialinda2007 <128740957+marialinda2007@users.noreply.github.com>
2023-10-17 09:16:06 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -11,6 +11,46 @@ function tocaSomPom(){
             const listaDeTeclas = document.querySelectorAll(tecla);
             listaDeTeclas[0].onclick = tocaSomPom
 
+            function tocaSomPom(){
+                document.querySelector('#som_tecla_pom').play();
+                }
+                const listaDeTeclas = document.querySelectorAll('.tecla');
+                listaDeTeclas[0].onclick = tocaSomPom;
+                listaDeTeclas[0].onclick = tocaSomPom;
+wlistaDeTeclas[0].onclick = tocaSomPom;
+while
+let contador = 0;
+// Estrutura de repetição - Enquanto
+let contador = 0;
+while(contador <8) 
+    while(contador <9) 
+        while(contador <9) {
+            listaDeTeclas[0].onclick = tocaSomPom;
+            }
+
+            contador = contador + 1;
+            listaDeTeclas[contador].onclick = tocaSomPom;
+            function tocaSomPom(){
+                document.querySelector('#som_tecla_pom').play();
+                }
+                while(contador < listaDeTeclas.length)
+                while(contador < listaDeTeclas.length) {
+                    listaDeTeclas[contador].onclick = tocaSomPom;
+                    contador = contador + 1;
+                    }
+                    function tocaSom(){
+                        document.querySelector('#som_tecla_pom').play();
+                        }
+                        function tocaSom(idElementoAudio)
+                            document.querySelector('#som_tecla_pom').play();
+                            document.querySelector(idElementoAudio).play();
+                            listaDeTeclas[contador].onclick = tocaSom;
+                            listaDeTeclas[contador].onclick = tocaSom;
+                            listaDeTeclas[contador].onclick = tocaSom('#som_tecla_toim');
+                            listaDeTeclas[contador].onclick = function () {
+                            };
+
+
             
 
 
